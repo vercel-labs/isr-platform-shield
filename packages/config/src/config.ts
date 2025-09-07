94b312e57fcca5341ab6e9bba919451e555bc3d2
Phil Zona <phil.zona@vercel.com>
2025-09-06 20:37:05 -0400

    what da...

--- packages/config/src/config.ts
+++ packages/config/src/config.ts
@@ -1,23 +1,27 @@
-const isProd = !!process.env.VERCEL;
-const protocol = isProd ? 'https' : 'http';
-
-export const config = {
-  api: {
-    url: isProd ? 'https://api.pzvtest314.vercel.app' : 'http://localhost:3002',
-  },
-  cacheLayer: {
-    url: isProd ? 'https://pzona.lol' : 'http://localhost:3000',
-  },
-  core: {
-    url: isProd ? 'https://core.pzvtest314.vercel.app' : 'http://localhost:3001',
-  },
+const globalEnv = {
+  isProd: () => !!process.env.VERCEL,
   redis: {
     url: process.env.REDIS_URL || '',
     restApiUrl: process.env.KV_REST_API_URL || '',
     restApiToken: process.env.KV_REST_API_TOKEN || '',
   },
+};
+
+const protocol = globalEnv.isProd() ? 'https' : 'http';
+
+export const config = {
+  api: {
+    url: globalEnv.isProd() ? 'https://api.pzvtest314.vercel.app' : 'http://localhost:3002',
+  },
+  cacheLayer: {
+    url: globalEnv.isProd() ? 'https://pzona.lol' : 'http://localhost:3000',
+  },
+  core: {
+    url: globalEnv.isProd() ? 'https://core.pzvtest314.vercel.app' : 'http://localhost:3001',
+  },
+  redis: globalEnv.redis,
   protocol,
-  rootDomain: isProd ? 'pzona.lol' : 'localhost:3000',
+  rootDomain: globalEnv.isProd() ? 'pzona.lol' : 'localhost:3000',
 };
 
 // Simple getters
