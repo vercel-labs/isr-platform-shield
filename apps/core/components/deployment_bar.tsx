280ee3c57f02829eca971824ac7c9a082d4a3bd7
Phil Zona <phil.zona@vercel.com>
2025-09-12 12:50:47 -0400

    Fix timestamps

--- apps/core/components/deployment_bar.tsx
+++ apps/core/components/deployment_bar.tsx
@@ -2,13 +2,13 @@ import { stringToColor } from "@/lib/deployment-id";
 
 export async function DeploymentBar() {
   const deploymentId = process.env.VERCEL_DEPLOYMENT_ID ?? 'local';
-  const deploymentDate = new Date().toLocaleString();
+  const deploymentDate = new Date().toLocaleString("en-US", { timeZone: "America/New_York" }); // sorry i need this
   const color = await stringToColor(`${deploymentId};${deploymentDate}`);
 
   return (
     <div className="px-3 py-1 text-sm text-background flex justify-between" style={{ backgroundColor: color }}>
       <span className="font-bold">
-        {new Date().toLocaleString()}
+        {deploymentDate}
       </span>
       <span>
       {deploymentId}
