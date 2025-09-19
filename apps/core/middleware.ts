55baae81d0da73642b79c75def737c963c053178
Phil Zona <phil.zona@vercel.com>
2025-09-19 16:09:54 -0400

    dont rewrite static assets

--- apps/core/middleware.ts
+++ apps/core/middleware.ts
@@ -26,14 +26,14 @@ export async function middleware(request: NextRequest) {
   return NextResponse.next();
 }
 
-// export const config = {
-//   matcher: [
-//     /*
-//      * Match all paths except for:
-//      * 1. /api routes
-//      * 2. /_next (Next.js internals)
-//      * 3. all root files inside /public (e.g. /favicon.ico)
-//      */
-//     '/((?!api|_next|[\\w-]+\\.\\w+).*)'
-//   ]
-// };
+export const config = {
+  matcher: [
+    /*
+     * Match all paths except for:
+     * 1. /api routes
+     * 2. /_next (Next.js internals)
+     * 3. all root files inside /public (e.g. /favicon.ico)
+     */
+    '/((?!api|_next|[\\w-]+\\.\\w+).*)'
+  ]
+};
