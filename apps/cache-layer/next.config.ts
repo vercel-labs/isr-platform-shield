f72d0f8efce64f29cfb5f0cc99953144aa2c454b
Phil Zona <phil.zona@vercel.com>
2025-09-21 00:00:28 -0400

    config

--- apps/cache-layer/next.config.ts
+++ apps/cache-layer/next.config.ts
@@ -7,14 +7,12 @@ const withToolbar = createWithVercelToolbar();
 const nextConfig: NextConfig = {
   rewrites: async () => {
     return {
-      // We do beforeFiles to make sure requests for static files are proxied to the core app
-      beforeFiles: [
-        {
-          source: "/:path*",
-          destination: `${process.env.NEXT_PUBLIC_PROTOCOL}://${process.env.CORE_HOST}/:path*`,
-        },
-      ],
-
+      // beforeFiles: [
+      //   {
+      //     source: "/:path*",
+      //     destination: `${process.env.NEXT_PUBLIC_PROTOCOL}://${process.env.CORE_HOST}/:path*`,
+      //   },
+      // ],
     };
   },
 };
