bbea147651036b38e23940efb2da431a1d7510ef
Phil Zona <phil.zona@vercel.com>
2025-09-20 23:40:20 -0400

    remove duplicate config

--- apps/cache-layer/next.config.ts
+++ apps/cache-layer/next.config.ts
@@ -5,23 +5,6 @@ import createWithVercelToolbar from '@vercel/toolbar/plugins/next';
 const withToolbar = createWithVercelToolbar();
 
 const nextConfig: NextConfig = {
-  headers: async () => {
-    return [
-      {
-        source: "/:path*",
-        headers: [
-          {
-            key: "x-vercel-enable-rewrite-caching",
-            value: "1"
-          },
-          {
-            key: "vercel-cdn-cache-control",
-            value: "s-maxage=30, stale-while-revalidate=31556952"
-          }
-        ]
-      },
-    ]
-  },
   rewrites: async () => {
     return {
       // We do beforeFiles to make sure requests for static files are proxied to the core app
