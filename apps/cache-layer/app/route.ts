c943989ae09b7c5360b038062e9ab835af5aac43
Phil Zona <phil.zona@vercel.com>
2025-09-22 22:47:31 -0700

    no cache on route handler

--- apps/cache-layer/app/route.ts
+++ apps/cache-layer/app/route.ts
@@ -3,5 +3,11 @@ import { NextRequest, NextResponse } from "next/server";
 export const dynamic = "force-dynamic";
 
 export async function GET(req: NextRequest) {
-  return NextResponse.json({ works: true })
+  const res = new NextResponse("hello /", {
+    status: 200,
+    headers: {
+      "Cache-Control": "no-store",
+    },
+  });
+  return res;
 }
