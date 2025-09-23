1f713211feb11b7298888df7d430289442c039b9
Phil Zona <phil.zona@vercel.com>
2025-09-23 13:49:02 -0700

    fallback?

--- apps/cache-layer/next.config.ts
+++ apps/cache-layer/next.config.ts
@@ -4,7 +4,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   rewrites: async () => {
     return {
-      beforeFiles: [
+      fallback: [
         {
           source: "/:path*",
           destination: `${process.env.NEXT_PUBLIC_PROTOCOL}://${process.env.CORE_HOST}/:path*`,
