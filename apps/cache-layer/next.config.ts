ec980099b6182272b0507504161ad575ad80bc9b
Phil Zona <phil.zona@vercel.com>
2025-09-23 00:05:58 -0700

    get back to working state

--- apps/cache-layer/next.config.ts
+++ apps/cache-layer/next.config.ts
@@ -2,25 +2,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  headers: async () => [
-    {
-      source: "/:path*",
-      headers: [
-        {
-          key: "x-vercel-enable-rewrite-caching",
-          value: "1"
-        },
-        {
-          key: "vercel-cdn-cache-control",
-          value: "s-maxage=30, stale-while-revalidate=31556952"
-        },
-        {
-          key: "x-vercel-bypass-protection",
-          value: `${process.env.VERCEL_AUTOMATION_BYPASS_SECRET}`
-        }
-      ]
-    },
-  ],
   rewrites: async () => {
     return [
       {
