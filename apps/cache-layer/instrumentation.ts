b73d02fc8993644b0d151e5e4f358da1a54811ae
Phil Zona <phil.zona@vercel.com>
2025-09-22 23:57:37 -0700

    Revert "remove app specific code from cache layer"
    This reverts commit c05f548d692a91a2e2bbd75b1da1c69fa244d96d.

---
+++ apps/cache-layer/instrumentation.ts
@@ -0,0 +1,13 @@
+import { registerOTel } from '@vercel/otel';
+
+// Force build: 0
+export function register() {
+  registerOTel({
+    serviceName: 'cache-layer',
+    instrumentationConfig: {
+      fetch: {
+        propagateContextUrls: []
+      }
+    }
+  });
+}
