a77a3a1c13772834b897ae88db8c0a2f31536eae
Phil Zona <phil.zona@vercel.com>
2025-09-23 13:14:04 -0700

    add beforeFiles back in

--- apps/cache-layer/next.config.ts
+++ apps/cache-layer/next.config.ts
@@ -3,12 +3,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   rewrites: async () => {
-    return [
-      {
-        source: "/:path*",
-        destination: `${process.env.NEXT_PUBLIC_PROTOCOL}://${process.env.CORE_HOST}/:path*`,
-      },
-    ]
+    return {
+      beforeFiles: [
+        {
+          source: "/:path*",
+          destination: `${process.env.NEXT_PUBLIC_PROTOCOL}://${process.env.CORE_HOST}/:path*`,
+        },
+      ]
+    }
   },
 };
 
