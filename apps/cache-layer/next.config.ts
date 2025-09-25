4dd5dfb93d7f9ee6159e882b9be0db51e325bee7
Phil Zona <phil.zona@vercel.com>
2025-09-25 14:25:17 -0700

    fix

--- apps/cache-layer/next.config.ts
+++ apps/cache-layer/next.config.ts
@@ -2,17 +2,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	rewrites: async () => {
-		return {
-			beforeFiles: [
-				{
-					source: "/:path*",
-					destination: `https://${process.env.CORE_HOST}/:path*`,
-				},
-			],
 
-		};
-	},
 };
 
 export default nextConfig;
