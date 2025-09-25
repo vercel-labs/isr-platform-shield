0acbf851b2378a1f7215f2c1d1cfffc9c25bc501
Phil Zona <phil.zona@vercel.com>
2025-09-25 12:35:41 -0700

    type error

--- apps/cache-layer/app/api/invalidate/route.ts
+++ apps/cache-layer/app/api/invalidate/route.ts
@@ -1,4 +1,4 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
   return NextResponse.json({ message: "not implememented" });
