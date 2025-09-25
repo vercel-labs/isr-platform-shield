e41bbc96b9bf3dd09dd0eb5d847fdf6ae97ed71e
Phil Zona <phil.zona@vercel.com>
2025-09-25 13:50:04 -0700

    fix loop pt 2

--- apps/cache-layer/next.config.ts
+++ apps/cache-layer/next.config.ts
@@ -4,19 +4,19 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
 	rewrites: async () => {
 		return {
-			beforeFiles: [
-				{
-					source: "/:path*",
-					destination: `${process.env.NEXT_PUBLIC_PROTOCOL}://${process.env.CORE_HOST}/:path*`,
-					has: [
-						{
-							type: "header",
-							key: "x-rewrite-to-core",
-							value: "1",
-						},
-					]
-				},
-			],
+			// beforeFiles: [
+			// 	{
+			// 		source: "/:path*",
+			// 		destination: `${process.env.NEXT_PUBLIC_PROTOCOL}://${process.env.CORE_HOST}/:path*`,
+			// 		has: [
+			// 			{
+			// 				type: "header",
+			// 				key: "x-rewrite-to-core",
+			// 				value: "1",
+			// 			},
+			// 		]
+			// 	},
+			// ],
 
 		};
 	},
