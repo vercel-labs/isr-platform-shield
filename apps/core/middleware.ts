60f4ac793f3bfe217ee89e7139921ae664a85e75
Phil Zona <phil.zona@vercel.com>
2025-09-25 15:39:41 -0700

    hope this works lol

--- apps/core/middleware.ts
+++ apps/core/middleware.ts
@@ -2,30 +2,30 @@ import { type NextRequest, NextResponse } from "next/server";
 import { extractSubdomain } from "./lib/subdomains";
 
 export async function middleware(request: NextRequest) {
-	const { pathname } = request.nextUrl;
-	const subdomain = extractSubdomain(request);
+	// const { pathname } = request.nextUrl;
+	// const subdomain = extractSubdomain(request);
 
-	const headers = new Headers(request.headers);
-	console.log("headers", headers);
+	// const headers = new Headers(request.headers);
+	// console.log("headers", headers);
 
-	if (subdomain && subdomain !== "www") {
-		// headers.set('vercel-cache-tag', subdomain);
+	// if (subdomain && subdomain !== "www") {
+	// 	// headers.set('vercel-cache-tag', subdomain);
 
-		// Block access to admin page from subdomains
-		if (pathname.startsWith("/admin")) {
-			return NextResponse.redirect(new URL("/", request.url));
-		}
+	// 	// Block access to admin page from subdomains
+	// 	if (pathname.startsWith("/admin")) {
+	// 		return NextResponse.redirect(new URL("/", request.url));
+	// 	}
 
-		// For the root path on a subdomain, rewrite to the subdomain page
-		if (pathname === "/") {
-			return NextResponse.rewrite(new URL(`/s/${subdomain}`, request.url));
-		}
+	// 	// For the root path on a subdomain, rewrite to the subdomain page
+	// 	if (pathname === "/") {
+	// 		return NextResponse.rewrite(new URL(`/s/${subdomain}`, request.url));
+	// 	}
 
-		// For all other paths on a subdomain, rewrite to that path
-		return NextResponse.rewrite(
-			new URL(`/s/${subdomain}${pathname}`, request.url),
-		);
-	}
+	// 	// For all other paths on a subdomain, rewrite to that path
+	// 	return NextResponse.rewrite(
+	// 		new URL(`/s/${subdomain}${pathname}`, request.url),
+	// 	);
+	// }
 
 	// On the root domain, allow normal access
 	return NextResponse.next();
