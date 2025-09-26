7c1a3bf3e2017315a6c80a4dca2cb7929c349955
Mark Knichel <7355009+mknichel@users.noreply.github.com>
2025-09-25 17:01:07 -0700

    force core build

--- apps/core/next.config.ts
+++ apps/core/next.config.ts
@@ -1,11 +1,9 @@
-// force build: 0
-import type { NextConfig } from "next";
-import createWithVercelToolbar from "@vercel/toolbar/plugins/next";
+// force build: 1
+import type { NextConfig } from 'next';
+import createWithVercelToolbar from '@vercel/toolbar/plugins/next';
 
 const withToolbar = createWithVercelToolbar();
 
-const nextConfig: NextConfig = {
-
-};
+const nextConfig: NextConfig = {};
 
 export default withToolbar(nextConfig);
