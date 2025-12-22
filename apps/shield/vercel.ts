51508c5458e4efec110aa89edd6b011c973f0f7e
Phil Zona <phil.zona@vercel.com>
2025-12-22 14:57:40 -0500

    use vercel.ts

---
+++ apps/shield/vercel.ts
@@ -0,0 +1,39 @@
+import { routes, deploymentEnv } from "@vercel/config/v1";
+import type { VercelConfig } from "@vercel/config/v1";
+
+// Could read these remotely, using env vars for simplicity
+const core = deploymentEnv("CORE_HOST");
+
+// Cache settings
+const sMaxAge = 120;
+const swr = 31556952;
+
+// Shield rewrite rules
+const shieldRewrites = [
+	["/api/shield/(.*)", "/api/$1"], // Shield internal API routes
+	["/", `https://${core}/s/$host`], // Root for tenants
+	["/((?!_next|api).*)", `https://${core}/s/$host/$1`], // Subdomain path for subdomains
+	["(.*)", `https://${core}/$1`], // Fallback route for subdomains
+].map(([src, dest], idx) =>
+	routes.rewrite(
+		src,
+		dest,
+		// Don't add headers to the internal API routes
+		idx !== 0 && {
+			has: [
+				{
+					type: "host",
+					value: "(?<host>.*)\\.[^\\.]+\\.[^\\.]+", // This capture group should be used in the destination URL
+				},
+			],
+			responseHeaders: {
+				"CDN-Cache-Control": `s-maxage=${sMaxAge}, stale-while-revalidate=${swr}`,
+			},
+		},
+	),
+);
+
+export const config: VercelConfig = {
+	framework: "nextjs",
+	rewrites: [routes.rewrite("/api/shield/(.*)", "/api/$1"), ...shieldRewrites],
+};
