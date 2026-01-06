cb151019bb9703336b3df288ee9ec7440fe3ec2a
Phil Zona <phil.zona@vercel.com>
2026-01-06 12:07:29 -0800

    hm

--- apps/shield/vercel.ts
+++ apps/shield/vercel.ts
@@ -10,32 +10,32 @@ const swr = 31556952;
 
 // Shield rewrite rules
 const shieldRewrites = [
-	["/api/shield/(.*)", "/api/$1"], // Shield internal API routes
-	["/", `https://${core}/s/$host`], // Root for tenants
-	["/((?!_next|api).*)", `https://${core}/s/$host/$1`], // Subdomain path for tenants
-	["(.*)", `https://${core}/$1`], // Fallback route for tenants
+  ["/api/shield/(.*)", "/api/$1"], // Shield internal API routes
+  ["/", `https://${core}/s/$host`], // Root for tenants
+  ["((?!_next|api).*)", `https://${core}/s/$host/$1`], // Subdomain path for tenants
+  ["(.*)", `https://${core}/$1`], // Fallback route for tenants
 ];
 
 export const config: VercelConfig = {
-	framework: "nextjs",
-	routes: shieldRewrites.map(([src, dest], idx) =>
-		routes.rewrite(
-			src,
-			dest,
-			// Don't add headers to the internal API routes
-			idx === 0
-				? { responseHeaders: { "x-noop": "0" } }
-				: {
-						has: [
-							{
-								type: "host",
-								value: "(?<host>.*)\\.[^\\.]+\\.[^\\.]+", // This capture group should be used in the destination URL
-							},
-						],
-						responseHeaders: {
-							"CDN-Cache-Control": `s-maxage=${sMaxAge}, stale-while-revalidate=${swr}`,
-						},
-					},
-		),
-	),
+  framework: "nextjs",
+  routes: shieldRewrites.map(([src, dest], idx) =>
+    routes.rewrite(
+      src,
+      dest,
+      // Don't add headers to the internal API routes
+      idx === 0
+        ? { responseHeaders: { "x-noop": "0" } }
+        : {
+            has: [
+              {
+                type: "host",
+                value: "(?<host>.*)\\.[^\\.]+\\.[^\\.]+", // This capture group should be used in the destination URL
+              },
+            ],
+            responseHeaders: {
+              "CDN-Cache-Control": `s-maxage=${sMaxAge}, stale-while-revalidate=${swr}`,
+            },
+          },
+    ),
+  ),
 };
