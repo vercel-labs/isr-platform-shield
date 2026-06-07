15cc4ae53f175138ecdc38a6a5facc31d6f37d1f
Phil Zona <phil.zona@vercel.com>
2026-06-07 15:24:30 -0400

    Instrumentation

--- apps/core/instrumentation.ts
+++ apps/core/instrumentation.ts
@@ -1,8 +1,25 @@
-import { OTLPHttpJsonTraceExporter, registerOTel } from "@vercel/otel";
+import { OTLPHttpProtoTraceExporter, registerOTel } from "@vercel/otel";
 
+const traceExporter = new OTLPHttpProtoTraceExporter({
+  url: `https://${process.env.AXIOM_DOMAIN}/v1/traces`,
+  headers: {
+    'Authorization': `Bearer ${process.env.AXIOM_TOKEN}`,
+    'X-Axiom-Dataset': 'shield-isr'
+  },
+});
+
+// Force build: 0
 export function register() {
 	registerOTel({
 		serviceName: "core",
-		instrumentationConfig: {},
+		instrumentationConfig: {
+			fetch: {
+				propagateContextUrls: [
+					process.env.CORE_URL || '',
+					process.env.SHIELD_URL || ''
+				]
+			}
+		},
+		traceExporter
 	});
 }
--- apps/shield/instrumentation.ts
+++ apps/shield/instrumentation.ts
@@ -1,4 +1,12 @@
-import { registerOTel } from "@vercel/otel";
+import { OTLPHttpProtoTraceExporter, registerOTel } from "@vercel/otel";
+
+const traceExporter = new OTLPHttpProtoTraceExporter({
+  url: `https://${process.env.AXIOM_DOMAIN}/v1/traces`,
+  headers: {
+    'Authorization': `Bearer ${process.env.AXIOM_TOKEN}`,
+    'X-Axiom-Dataset': 'shield-isr'
+  },
+});
 
 // Force build: 0
 export function register() {
@@ -6,8 +14,12 @@ export function register() {
 		serviceName: "shield",
 		instrumentationConfig: {
 			fetch: {
-				propagateContextUrls: [],
-			},
+				propagateContextUrls: [
+					process.env.CORE_URL || '',
+					process.env.SHIELD_URL || ''
+				]
+			}
 		},
+		traceExporter
 	});
 }
