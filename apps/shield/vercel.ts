e2534b8583741c3139692f6519966b58107716f6
Phil Zona <phil.zona@vercel.com>
2026-06-07 23:05:15 -0400

    Revert, use old syntax

--- apps/shield/vercel.ts
+++ apps/shield/vercel.ts
@@ -1,13 +1,17 @@
-import { routes, deploymentEnv } from "@vercel/config/v1";
 import type { VercelConfig } from "@vercel/config/v1";
+import { deploymentEnv, routes } from "@vercel/config/v1";
 
 // Request header needed to bypass deployment protection
-const bypassHeader = { "x-vercel-protection-bypass": deploymentEnv("VERCEL_BYPASS_TOKEN") }
+const bypassHeader = {
+  "x-vercel-protection-bypass": deploymentEnv("VERCEL_BYPASS_TOKEN"),
+};
 
 // Cache settings and response header to add for relevant routes
 const sMaxAge = 120;
 const swr = 31556952;
-const cacheHeader = { "CDN-Cache-Control": `s-maxage=${sMaxAge}, stale-while-revalidate=${swr}` }
+const cacheHeader = {
+  "CDN-Cache-Control": `s-maxage=${sMaxAge}, stale-while-revalidate=${swr}`,
+};
 
 // Base url for rewrites
 const core = deploymentEnv("CORE_HOST");
@@ -21,24 +25,24 @@ const rewrites = {
     ["/_next/(.*)", `https://${core}/_next/$1`], // Next.js assets - no host condition needed
   ],
   withCapturedHost: [
-    ["/", `https://${core}/s/:host`], // Root for tenants
-    ["/((?!_next/|api/|s/).+)", `https://${core}/s/:host/$1`], // Subdomain path for tenants
+    ["/", `https://${core}/s/$host`], // Root for tenants
+    ["/((?!_next/|api/|s/).+)", `https://${core}/s/$host/$1`], // Subdomain path for tenants
   ],
   fallback: [
     ["(.*)", `https://${core}/$1`], // Fallback route for tenants
-  ]
-}
+  ],
+};
 
 // Exported project config
 export const config: VercelConfig = {
   framework: "nextjs",
   routes: [
-    ...rewrites.withoutCapturedHost.map(([src, dest]) => (
+    ...rewrites.withoutCapturedHost.map(([src, dest]) =>
       routes.rewrite(src, dest, {
-        requestHeaders: { ...bypassHeader }
-      })
-    )),
-    ...rewrites.withCapturedHost.map(([src, dest]) => (
+        requestHeaders: { ...bypassHeader },
+      }),
+    ),
+    ...rewrites.withCapturedHost.map(([src, dest]) =>
       routes.rewrite(src, dest, {
         has: [
           {
@@ -48,12 +52,12 @@ export const config: VercelConfig = {
         ],
         requestHeaders: { ...bypassHeader },
         responseHeaders: { ...cacheHeader },
-      })
-    )),
-    ...rewrites.fallback.map(([src, dest]) => (
+      }),
+    ),
+    ...rewrites.fallback.map(([src, dest]) =>
       routes.rewrite(src, dest, {
-        requestHeaders: { ...bypassHeader }
-      })
-    )),
-  ]
+        requestHeaders: { ...bypassHeader },
+      }),
+    ),
+  ],
 };
